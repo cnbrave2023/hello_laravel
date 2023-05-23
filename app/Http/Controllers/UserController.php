3bbd4c9970cc09d50bf6636c40f31709f6024250
cnBrave <zhy120@qq.com>
2023-05-23 16:02:31 +0000

    完成用户注册功能

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,8 +23,16 @@ class UserController extends Controller
         $this->validate($request,[
             'name'=>'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min:1'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('user.show', [$user]);
     }
 }
